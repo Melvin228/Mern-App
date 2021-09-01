311289df04241aa3e6eb7f261ffaf1d59199afc1
Melvin Gan <melvingan2811@gmail.com>
2021-09-01 22:05:56 +0800

    Added ui for register page and added state using hooks

---
+++ client/src/Components/Hooks/useForm.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const useForm = (initialState) => {
+  const [values, setValues] = useState(initialState);
+
+  return [
+    values,
+    (e) => {
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value,
+      });
+    },
+  ];
+};
+
+export default useForm;
--- client/src/Components/auth/Register.js
+++ client/src/Components/auth/Register.js
@@ -1,11 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import useForm from "../Hooks/useForm";
 
-const register = () => {
+const Register = () => {
+  const [values, handleChange] = useForm({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("HELLO");
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="register">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Sign up</h1>
+            <p className="lead text-center">Create your devConnector account</p>
+            <form onSubmit={submitHandler}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Name"
+                  name="name"
+                  value={values.name}
+                  onChange={handleChange}
+                />
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email address"
+                    value={values.email}
+                    name="email"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    value={values.password}
+                    name="password"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm password"
+                    value={values.password2}
+                    name="password2"
+                    onChange={handleChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </div>
+            </form>
+            <h1>Testing</h1>
+            <h2>{values.name}</h2>
+            <h2>{values.email}</h2>
+            <h2>{values.password}</h2>
+            <h2>{values.password2}</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default register;
+export default Register;
